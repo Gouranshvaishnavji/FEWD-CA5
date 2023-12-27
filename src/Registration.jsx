247c3c294f3b2d4b4125af4b02c2026e3d33fbf4
Gouransh <147132368+Gouranshvaishnavji@users.noreply.github.com>
2023-12-27 12:41:43 +0530

    Update Registration.jsx is it last

--- src/Registration.jsx
+++ src/Registration.jsx
@@ -2,147 +2,160 @@ import React, { useState, useEffect } from "react";
 import "./registration.css";
 import { useNavigate } from "react-router-dom";
 
-function Registration() {
+function RegistrationUser() {
+  // Initial form values
   const initialValues = { username: "", email: "", password: "", confirmPassword: "" };
+  
+  // State to manage form values
   const [formValues, setFormValues] = useState(initialValues);
-  const [formErrors, setFormErrors] = useState({});
-  const [isSubmit, setIsSubmit] = useState(false);
+
+  // State to manage form errors
+  const [formErrors, setAllFormErrors] = useState({});
+
+  // State to track form submission status
+  const [isSubmit, setSubmitDone] = useState(false);
+
+  // React Router navigation hook
   const navigate = useNavigate();
 
-  const handleChange = (e) => {
+  // Function to handle form input changes
+  const handlingChanges = (e) => {
     const { name, value } = e.target;
     setFormValues({ ...formValues, [name]: value });
   };
 
-  const handleSubmit = (e) => {
+  // Function to handle form submission
+  const handlingSubmitions = (e) => {
     e.preventDefault();
-    setFormErrors(validate(formValues));
-    setIsSubmit(true);
+    setAllFormErrors(validate(formValues));
+    setSubmitDone(true);
   };
 
+  // Effect to navigate when there are no form errors and the form has been submitted
   useEffect(() => {
     console.log(formErrors);
     if (Object.keys(formErrors).length === 0 && isSubmit) {
-      console.log(formValues);
-      navigate("/", { state: { successMessage: "Registration successful!" } });
+      navigate("/", { state: { successMessage: "successful!" } });
     }
   }, [formErrors]);
 
+  // Function to validate form values
   const validate = (values) => {
     const errors = {};
     const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+    
     if (!values.username) {
       errors.username = "Username is required!";
-    } else if (values.username.length < 3) {
-      errors.username = "Username must be more than 3 characters";
-    } else if (values.username.length > 30) {
-      errors.username = "Username cannot exceed more than 30 characters";
+    } else if (values.username.length > 20) {
+      errors.username = "Username cannot exceed more than 20 characters";
     }
+
     if (!values.email) {
       errors.email = "Email is required!";
     } else if (!regex.test(values.email)) {
       errors.email = "This is not a valid email format!";
     }
+
     if (!values.password) {
       errors.password = "Password is required";
     } else if (values.password.length < 10) {
       errors.password = "Password must be more than 10 characters";
-    } else if (values.password.length > 15) {
-      errors.password = "Password cannot exceed more than 10 characters";
-    }
+    } 
+
     if (!values.confirmPassword) {
       errors.confirmPassword = "Please confirm your password";
     } else if (values.confirmPassword !== values.password) {
-      errors.confirmPassword = "Passwords do not match";
+      errors.confirmPassword = "Given ID/Password is not Correct";
     }
+
     return errors;
   };
 
+  // JSX structure of the component
   return (
-    <div className="container">
+    <div className="main">
+      {/* Uncomment below to show success message or form values in pre tag */}
       {/* {Object.keys(formErrors).length === 0 && isSubmit ? (
         <div className="success-message">Signed in successfully</div>
       ) : (
         <pre>{JSON.stringify(formValues, undefined, 2)}</pre>
       )} */}
 
-      <form className="form-container" onSubmit={handleSubmit}>
-        <h1 className="form-header">Registration Form</h1>
+      <form className="form-main" onSubmit={handlingSubmitions}>
+        <h1 className="form-top">Registration Form</h1>
         
-        <div className="form-group">
-          <label className="form-label" htmlFor="username">
-            Username
+        {/* Form input fields for username */}
+        <div className="forms">
+          <label id="forms_Instr" htmlFor="username">
+            Yourname <span id="username">"</span>
           </label>
           <input
-            type="text"
-            id="username"
-            name="username"
-            className="form-input"
-            placeholder="Username"
-            value={formValues.username}
-            onChange={handleChange}
+            type="text" id="username" name="username" className="input" placeholder="Yourname" value={formValues.username}
+            onChange={handlingChanges}
           />
           <p className="error-message" htmlFor="username">
             {formErrors.username}
           </p>
         </div>
 
-        <div className="form-group">
-          <label className="form-label" htmlFor="email">
+        {/* Form input fields for email */}
+        <div className="forms">
+          <label id="forms_Instr" htmlFor="email">
             Email
           </label>
           <input
             type="text"
             id="email"
             name="email"
-            className="form-input"
-            placeholder="Email"
-            value={formValues.email}
-            onChange={handleChange}
+            className="input" placeholder="Email" value={formValues.email}
+            onChange={handlingChanges}
           />
           <p className="error-message" htmlFor="email">
             {formErrors.email}
           </p>
         </div>
 
-        <div className="form-group">
-          <label className="form-label" htmlFor="password">
+        {/* Form input fields for password */}
+        <div className="forms">
+          <label id="forms_Instr" htmlFor="password">
             Password
           </label>
           <input
             type="password"
             id="password"
             name="password"
-            className="form-input"
+            className="input"
             placeholder="Password"
             value={formValues.password}
-            onChange={handleChange}
+            onChange={handlingChanges}
           />
           <p className="error-message" htmlFor="password">
             {formErrors.password}
           </p>
         </div>
 
-        <div className="form-group">
-          <label className="form-label" htmlFor="confirmPassword">
+        {/* Form input fields for confirming password */}
+        <div className="forms">
+          <label id="forms_Instr" htmlFor="confirmPassword">
             Confirm Password
           </label>
           <input
             type="password"
             id="confirmPassword"
             name="confirmPassword"
-            className="form-input"
+            className="input"
             placeholder="Confirm Password"
             value={formValues.confirmPassword}
-            onChange={handleChange}
+            onChange={handlingChanges}
           />
           <p className="error-message" htmlFor="confirmPassword">
             {formErrors.confirmPassword}
           </p>
         </div>
 
-        <div className="form-group">
-          <button className="submit-button" type="submit">
+        {/* Form submit button */}
+        <div className="forms">
+          <button className="submit" type="submit">
             Sign up
           </button>
         </div>
@@ -151,4 +164,4 @@ function Registration() {
   );
 }
 
-export default Registration;
+export default RegistrationUser;
