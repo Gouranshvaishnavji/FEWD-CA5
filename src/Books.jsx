35750d9e9a3f9c37c40d848dd25914163757763a
Gouransh <147132368+Gouranshvaishnavji@users.noreply.github.com>
2023-12-27 13:58:42 +0530

    Update Books.jsx is it last

--- src/Books.jsx
+++ src/Books.jsx
@@ -3,59 +3,89 @@ import axios from 'axios';
 import { useLocation } from 'react-router-dom';
 import './books.css';
 
-
 function Books() {
-  const [books, setBooks] = useState([]);
-  const [filteredBooks, setFilteredBooks] = useState([]);
+  // State to hold all books
+  const [books, gotBooks] = useState([]);
+  
+  // State to hold books after search/filter
+  const [filteredBooks, setSearchedBooks] = useState([]);
+  
+  // React Router hook to get location state
   const location = useLocation();
+  
+  // Extract success message from location state
   const successMessage = location?.state?.successMessage;
 
+  // Fetch books from the API on component mount
   useEffect(() => {
-    const fetchBooks = async () => {
+    const getBooks = async () => {
       try {
         const response = await axios.get('https://reactnd-books-api.udacity.com/books', {
           headers: {
             'Authorization': 'whatever-you-want'
           }
         });
-        console.log(response);
-        const fetchedBooks = response.data.books;
-        setBooks(fetchedBooks);
-        setFilteredBooks(fetchedBooks);      
-    } catch (error) {
+        const allBooks = response.data.books;
+        gotBooks(allBooks);
+        setSearchedBooks(allBooks);      
+      } catch (error) {
         console.error('Error fetching books:', error);
       }
     };
-    fetchBooks();
+    getBooks();
   }, []);
-const filtersearch = (e) => {
-  const query = e.target.value.toLowerCase();
-    const filtered = books.filter((book) =>
-      book.title.toLowerCase().includes(query)
-    );
-    setFilteredBooks(filtered);
-  
-};
 
+  // Function to filter books based on search input
+  const filteronsearch = (e) => {
+    const input = e.target.value.toLowerCase();
+    const searched = books.filter((book) =>
+      book.title.toLowerCase().includes(input)
+    );
+    setSearchedBooks(searched);
+  };
+
+  // JSX structure of the component
   return (
     <>
-    <input id='search' type="text" placeholder="Search Books"  onChange={filtersearch} />
-    <div id='success'>
-        {successMessage?<p>{successMessage}</p>:<p>Register to get free books</p>}
-    </div>
-    <div className="book-container" id="book-container">
+      {/* Search input box */}
+      <input id='search-box' type="text" placeholder="Search Books with Title kudasai !" onChange={filteronsearch} />
+
+      {/* Displaying filtered books */}
+      <div className="book-main">
         {filteredBooks.map((book) => (
-        <div className="book" key={book.id}>
-            <img className="book-thumbnail" src={book.imageLinks.thumbnail} alt={book.title} />
-            <h2 className="book-title">
-            <a href={book.previewLink}>{book.title}</a>
+          <div className="allbook" key={book.id}>
+            {/* Book image */}
+            <img className="book-img" src={book.imageLinks.thumbnail} alt={book.title} />
+            
+            {/* Book title with link */}
+            <h2 className="title">
+              <a href={book.previewLink}>{book.title}</a>
             </h2>
-            <h3 className="book-authors">By {book.authors.map((author) => <div key={author}>{author}</div>)}</h3>
-            {/* <div className='additional'><div className='rating'>{getStarRating(book.averageRating)}</div><div className='status'>{successMessage?'Free':'Buy'}</div></div> */}
-        </div>
+            
+            {/* Book authors */}
+            <h3 className="by">
+              By {book.authors.map((author) => 
+                <div key={author}>
+                  {author}
+                </div>
+              )}
+            </h3>
+            {/* Additional book information (commented out) */}
+          </div>
         ))}
-    </div>
+      </div>
+
+      {/* Display message based on registration status */}
+      <div id='Registration-done'>
+        {successMessage ? <p>{successMessage}</p> : <p>Register to get free books</p>}
+      </div>
+
+      {/* Footer */}
+      <footer id='footer'>
+        <p>&copy; 2023. All rights reserved. made by Gouransh Vaishnav</p>
+      </footer>
     </>
   );
 }
+
 export default Books;
